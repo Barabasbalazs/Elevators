974dff6211c72b2e601eaff1c64fe8595b60cfc3
Barabás Balázs <barabas.balazska@gmail.com>
2023-01-05 18:02:55 +0200

    Setup vite + react for project

---
+++ elevator-project/src/views/ElevatorApp.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import '../styles/ElevatorApp.css'
+
+const ElevatorApp = () => {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="flex justify-center">
+      <div className="w-2/3 my-10 h-screen">
+        {for ()}
+      </div>
+    </div>
+  )
+}
+
+export default ElevatorApp;
