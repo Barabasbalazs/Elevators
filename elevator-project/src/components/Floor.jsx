bfaeeae06ea2eac175dede9263913df400c08342
Barabás Balázs <barabas.balazska@gmail.com>
2023-01-07 16:59:07 +0200

    Elevators finally move accordingly

--- elevator-project/src/components/Floor.jsx
+++ elevator-project/src/components/Floor.jsx
@@ -4,10 +4,7 @@ import ElevatorComp from './ElevatorComp';
 
 const Floor = (props) => {
 
-    console.log(`Floor : ${props.index} left: ${props.left} && right: ${props.right}`);
-
     const [floors] = useState(props.floors.reverse());
-    // const [currentFloor] = useState(props.index);
 
     const buttonClicked = (e) => {
         props.floorCallBack(props.index, e.target.value);
@@ -21,7 +18,7 @@ const Floor = (props) => {
                 <Button value={'down'} onClick={buttonClicked}>Down</Button>
             </div>
             <div className="h-full w-full">
-            {props.index === props.left && 
+            {props.floorSituation.left && 
                 <ElevatorComp floors={floors} side={'left'}
                     currentFloor={props.index}
                     buttonClickCallback={props.elevatorCallBack}
@@ -29,7 +26,7 @@ const Floor = (props) => {
             }
             </div>
             <div className="h-full w-full border-l-4">
-            {props.index === props.right && 
+            {props.floorSituation.right && 
                  <ElevatorComp floors={floors} side={'right'}
                     currentFloor={props.index}
                     buttonClickCallback={props.elevatorCallBack}
--- elevator-project/src/views/ElevatorApp.jsx
+++ elevator-project/src/views/ElevatorApp.jsx
@@ -6,31 +6,144 @@ const ElevatorApp = () => {
 
   const [floors, setFloors] = useState([6, 5, 4, 3, 2, 1, 0])
 
-  const [left, setLeft] = useState({
-    pos: 0
-  })
+  const [floorStates, setFloorStates] = useState([
+    {
+      left: true,
+      right: false,
+    },
+    {
+      left: false,
+      right: false,
+    },
+    {
+      left: false,
+      right: false,
+    },
+    {
+      left: false,
+      right: false,
+    },
+    {
+      left: false,
+      right: false,
+    },
+    {
+      left: false,
+      right: false,
+    },
+    {
+      left: false,
+      right: true,
+    }
+  ])
 
-  const [right, setRight] = useState({
-    pos: 6
-  })
+  const [left, setLeft] = useState(0)
 
-  const floorButtonPush = (floorNumber, buttonClicked) => {
-    console.log(`On floor = ${floorNumber} clicked button = ${buttonClicked}`);
+  const [right, setRight] = useState(6)
+
+  const changeLeft = (buttonClicked) => {
+    setFloorStates((prevFloorStates) => {
+      const newFloorState = prevFloorStates.map((el, ind) => {
+        if (ind === parseInt(buttonClicked)) {
+          return {
+            left: true,
+            right: el.right
+          };
+        } else if (el.left === true) {
+          return {
+            left: false,
+            right: el.right
+          };
+        } else {
+          return el;
+        }
+      })
+      return newFloorState;
+    })
+  }
+
+  const changeRight = (buttonClicked) => {
+    setFloorStates((prevFloorStates) => {
+      const newFloorState = prevFloorStates.map((el, ind) => {
+        if (ind === parseInt(buttonClicked)) {
+          return {
+            left: el.left,
+            right: true
+          };
+        } else if (el.right === true) {
+          return {
+            left: el.left,
+            right: false
+          };
+        } else {
+          return el;
+        }
+      })
+      return newFloorState;
+    })
   }
 
   const elavatorButtonPush = async (side, floorPos, buttonClicked) => {
     if (side === 'left') {
-      console.log(`Left is going: ${buttonClicked}`);
-      setLeft({
-        pos: buttonClicked
-      });
+      if (buttonClicked > left) {
+        for (let i = left; i <= buttonClicked; i++) {
+          setTimeout(() => {
+            setLeft(i);
+            changeLeft(i);
+          }, 1000 * i);
+        }
+      } else if (buttonClicked < left) {
+        const div = (left - buttonClicked);
+        for (let i = 0; i <= div; i++) {
+          setTimeout(() => {
+            setLeft(left - i);
+            changeLeft(left - i); 
+          }, 1000 * i);
+        }
+      }
     } else if (side === 'right') {
-      console.log(`Right is going: ${buttonClicked}`);
-      setRight({
-        pos: buttonClicked
-      });
+      if (buttonClicked > right) {
+        for (let i = right; i <= buttonClicked; i++) {
+          setTimeout(() => {
+            setRight(i);
+            changeRight(i);
+          }, 1000 * i);
+        }
+      } else if (buttonClicked < right) {
+        const div = (right - buttonClicked);
+        for (let i = 0; i <= div; i++) {
+          setTimeout(() => {
+            setRight(right - i);
+            changeRight(right - i); 
+          }, 1000 * i);
+        }
+      }
     }
   } 
+  
+  const calculateClosest = (floorNumber) => {
+    if (Math.abs(left - floorNumber) < Math.abs(right - floorNumber)) {
+      return 'left';
+    } else if (Math.abs(left - floorNumber) > Math.abs(right - floorNumber)) {
+      return 'right';
+    } else if (Math.abs(left - floorNumber) > Math.abs(right - floorNumber)) {
+      if (left < floorNumber) {
+        return 'left';
+      } else if (right < floorNumber) {
+        return 'right';
+      }
+    }
+  }
+
+  const floorButtonPush = (floorNumber, buttonClicked) => {
+    if (calculateClosest(floorNumber) === 'left') {
+      setLeft(floorNumber);
+      changeLeft(floorNumber);
+    } else {
+      setRight(floorNumber);
+      changeRight(floorNumber);
+    }
+  }
 
   return (
     <div className="flex justify-center">
@@ -40,12 +153,9 @@ const ElevatorApp = () => {
                     index={index}
                     floors={floors}
                     key={index}
-                    leftOnFloor={true}
-                    rightOnFloor={true}
                     elevatorCallBack={elavatorButtonPush}
                     floorCallBack={floorButtonPush}
-                    right={right.pos}
-                    left={left.pos}
+                    floorSituation={floorStates[index]}
                     />; }
         )}
       </div>
